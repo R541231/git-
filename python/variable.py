fdd4c1806336644f59a86a949414c6e30561d68f
Ravish Singh <hravishsingh@gmail.com>
2021-04-26 23:58:11 +0530

    skip staging areapython/variable.py

--- python/variable.py
+++ python/variable.py
@@ -1,12 +1,24 @@
-# var="25"
-# var1=121
-# var2=12.33
-# # print(int(var)+var1)
-# print("Enter a number")
+# # var="25"
+# # var1=121
+# # var2=12.33
+# # # print(int(var)+var1)
+# # print("Enter a number")
+# # a=input()
+# # print("you have entered a number",a)
+# # print("Enter 1'st Number")1
+# # print("Enter 2'st Number")
+# # a=input()
+# # b=input()
+# # print("Sum of two numbers",int(a)+int(b))
+# print("Enter a first number")
 # a=input()
-# print("you have entered a number",a)
-print("Enter 1'st Number")1
-print("Enter 2'st Number")
-a=input()
-b=input()
-print("Sum of two numbers",int(a)+int(b))
+# print("Enter a second number")
+# b=input()
+# print("multiple of numbers",int(a)*int(b))
+a = "Hello world there is my new vlog!"
+# print(len(a))
+# print(a[0:3])
+# print(a[::-2])
+# print(a.isalpha())
+print(a.find("new"))
+
