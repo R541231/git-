03b09611e2e89861baba60d3be0ebc05956f889f
Ravish Singh <hravishsingh@gmail.com>
2021-04-26 23:59:20 +0530

    skip staging area

---
+++ python/list.py
@@ -0,0 +1,7 @@
+groceries =("aalo","kela","seb","aata","maida","honey","doodh",56,54)
+print(groceries[0])
+numbers =[2,20,4,5,6,3,1]
+# numbers.sort()
+# numbers.reverse()
+numbers.__getattribute__
+print(numbers)
