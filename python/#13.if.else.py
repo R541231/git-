0ac8baf9e990e44e1b919808a9bd73bd9910fea2
Ravish Singh <hravishsingh@gmail.com>
2021-05-01 00:59:18 +0530

    skip staging

--- python/#13.if.else.py
+++ python/#13.if.else.py
@@ -8,3 +8,50 @@
 #     print("Equal")
 # else:
 #     print("Lesser")
+
+
+# list=[69,50,100]
+# print(66 in list)
+# if 66 in list:
+#     print("yes,the is correct")
+
+
+
+# print("Enter you age")
+
+# a=input()
+# var=int(a)
+# # if var>100:
+#     # print("Soory,This is not perfect age")
+# if var>18 and var<100:
+#     print("Yes,you can Drive")
+# elif var==18:
+#     print("Please come physically to know your driving status")
+# elif var<18 :
+#     print("No,you can't Drive")
+# else:
+#     print("This is not perfect age")
+
+
+
+
+
+
+
+
+
+
+var1 = 19
+var2 = 18
+var3 = int(input("Tell your real age: "))
+if var3>var2 and var3<100:
+    print("You can drive with us")
+elif var3==var2:
+    print("Your age is 18 we cannot deside")
+elif var3<var2:
+    print("no, you can't drive")
+else:
+    print("this is not perfect age")
+
+
+
