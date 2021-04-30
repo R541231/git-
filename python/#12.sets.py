5ed09f147e46938d7b3aa466356ca7d9f27eceda
Ravish Singh <hravishsingh@gmail.com>
2021-05-01 00:13:40 +0530

    skip staging area and commit these files

---
+++ python/#12.sets.py
@@ -0,0 +1,12 @@
+a=set()
+# sets=[1,5,5,8,6,9,5,2]
+# a=set(sets)
+# print(type(a))
+
+a.add(55)
+a.add(53)
+# print(a)
+# print(type(a))
+b=a.union({55,53,41})
+
+print(b)
---
+++ python/#13.if.else.py
@@ -0,0 +1,10 @@
+# var=5
+# var2=65
+# a=input()
+# var3=int(a)
+# if var3>var2:
+#     print("Greater")
+# elif var3==var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
